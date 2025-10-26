159e1c22e69f4abcad5e78be7823539c11cd5bf6
sunghun <nokdonwithkj211@gmail.com>
2025-10-26 23:10:03 +0900

    Feat(CarSignal): add CarSignal representing moving condition(Random number)

---
+++ src/main/java/racingcar/CarSignal.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class CarSignal {
+    private final int carSignalValue;
+    private static final int STANDARD = 4;
+
+    public CarSignal(int carSignalValue) {
+        this.carSignalValue = carSignalValue;
+    }
+
+    public boolean isPermitted() {
+        return carSignalValue >= STANDARD;
+    }
+}
