ae08fcddb32c9f8278155959c1a47140dcf6cb88
sunghun <nokdonwithkj211@gmail.com>
2025-10-27 10:55:41 +0900

    Feat(Application): plan for overall flow of application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.service.RacingService;
+import racingcar.util.RandomNumberProvider;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame(
+                new InputView(), new OutputView(),
+                new RacingService(new RandomNumberProvider())
+        );
+
+        racingGame.registerConfig();
+        racingGame.run();
+        racingGame.printResult();
     }
 }
