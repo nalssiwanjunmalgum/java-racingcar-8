1e1d151d258ac27a852d1c9d02a6c7d9ea4fec79
sunghun <nokdonwithkj211@gmail.com>
2025-10-27 10:54:29 +0900

    Feat(RacingGame): plan the flow of program overall

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,4 +1,34 @@
 package racingcar;
 
+import racingcar.service.RacingService;
+
 public class RacingGame {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+
+    public RacingGame(InputView inputView, OutputView outputView, RacingService racingService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+    }
+
+    public void registerConfig() {
+        String carNamesInput = inputView.getCarNamesInput();
+        String roundInput = inputView.getRoundInput();
+        racingService.registerInfo(roundInput, carNamesInput);
+    }
+
+    public void run() {
+        outputView.printLine();
+        outputView.printStatusMessage();
+
+        while (racingService.hasMoreRound()) {
+            outputView.printStatus(racingService.executePerRound());
+        }
+    }
+
+    public void printResult() {
+        outputView.printWinner(racingService.getWinner());
+    }
 }
--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,4 +1,46 @@
-package racingcar;
+package racingcar.service;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.RacingCars;
+import racingcar.domain.vo.RandomCarSignals;
+import racingcar.domain.vo.Round;
+import racingcar.util.NumberProvider;
 
 public class RacingService {
+    private final NumberProvider numberProvider;
+    private Round round;
+    private RacingCars racingCars;
+
+    public RacingService(NumberProvider numberProvider) {
+        this.numberProvider = numberProvider;
+    }
+
+    public void registerInfo(String roundInput, String carNamesInput) {
+        this.round = registerRound(roundInput);
+        this.racingCars = registerCarNames(carNamesInput);
+    }
+
+    public boolean hasMoreRound() {
+        return round.isLeft();
+    }
+
+    public RacingCars executePerRound() {
+        round = round.decrease();
+        List<Integer> providedNumbers = numberProvider.provideNumber(racingCars.getSize());
+        racingCars.receiveSignals(new RandomCarSignals(providedNumbers));
+        return racingCars;
+    }
+
+    public List<Car> getWinner() {
+        return racingCars.findWinner();
+    }
+
+    private Round registerRound(String input) {
+        return new Round(Long.parseLong(input));
+    }
+
+    private RacingCars registerCarNames(String input) {
+        return new RacingCars(input);
+    }
 }
